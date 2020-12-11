c14d402fab20a367a12f30a6e13e6e3d2b0b045c
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2020-12-12 00:28:29 +0800

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+icon_dir=$1
+PWA_PREFIX=$2
+APP[0]="opera"
+APP[1]="firefox"
+case $1 in
+  -h)
+       echo "Run 'pwashortcut -h' for more information"
+       echo "Run 'pwashortcut -i' for available preinstalled icons"
+       ;;
+  -i)
+       echo "Available preinstalled themes:"
+esac
+################################################
+echo "Making a customize PWA configuration"
+read -p "Application Name:" appname
+if [[ '${APP[*]}' == '$appname' ]] 
+then
+    read -r -p "$appname has a preinstalled customize theme, do you wanna use it? [y/N] " appname_response
+    case "$appname_response" in
+      [yY][eE][sS]|[yY]) 
+        cp $icon_dir/$appname.json $PWA_PREFIX/$appname/templates/manifest.json
+        echo "Using preinstalled configuration"
+        break
+        ;;
+      *)
+        break
+        ;;
+    esac
+fi
+read -p "Enter path of the icon you want to use in ${appname^}:" icon_path
+read -p "Enter the window color of PWA you want to use in HEX (without #):" window_color
+read -p "Enter the background color of PWA you want to use in HEX (without #):" background_color
+cp $icon_path $PWA_PREFIX/$appname/brew.png
+sed 's/000000/$window_color/1' $PWA_PREFIX/$appname/manifest.json
+sed 's/000000/$background_color/2' $PWA_PREFIX/$appname/manifest.json
+echo "A customize PWA configuration file for ${appname^} created!"
