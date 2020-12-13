c42a95d5aa786e7bc9fd184e4b06c4a2f720386b
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2020-12-13 14:40:37 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+curl https://github.com/supechicken666/pwashortcut/archive/testing.tar.gz -#o test.tgz
+tar xvf test.tgz
+mkdir -p /usr/local/lib/pwa/tools/
+mkdir -p /usr/local/lib/pwa/templates/
+cp installer.html /usr/local/lib/pwa/templates/
+cp main.py /usr/local/lib/pwa/
+mv * /usr/local/lib/pwa/tools/
+echo "Installed"
