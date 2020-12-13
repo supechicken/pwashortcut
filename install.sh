0b1e969572406cb336dbdaac292f5ba1a392363e
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2020-12-13 15:12:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ mkdir -p /usr/local/lib/pwa/tools/
 mkdir -p /usr/local/lib/pwa/templates/
 cp pwashortcut-testing/installer.html /usr/local/lib/pwa/templates/
 cp pwashortcut-testing/main.py /usr/local/lib/pwa/
-mv * /usr/local/lib/pwa/tools/
+cp -r pwashortcut-testing/* /usr/local/lib/pwa/tools/
 ruby pwashortcut-testing/main.rb
 echo "Installed"
