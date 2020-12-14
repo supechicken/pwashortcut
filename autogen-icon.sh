7a0b139fafa8248f97782db33fda0d125f7357bf
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2020-12-14 16:10:05 +0800

    Stable update (#3)
    * Create install.sh
    
    * Create main.py
    
    * Create installer.html
    
    * Create main.rb
    
    * Update install.sh
    
    * Update main.rb
    
    * Update install.sh
    
    * Update install.sh
    
    * Update install.sh
    
    * Update install.sh
    
    * Update main.rb
    
    * Update main.rb
    
    * Create manifest.json.bak
    
    * Update main.rb
    
    * Add files via upload
    
    * Update install.sh
    
    * Update main.rb
    
    * Add files via upload (#2)
    
    * Update main.rb
    
    * Update main.rb
    
    * Update find.sh
    
    * Update autogen-icon.sh
    
    * Update autogen-icon.sh
    
    * Update autogen-icon.sh
    
    * Update find.sh
    
    * Update autogen-icon.sh
    
    * Update main.rb
    
    * Update find.sh
    
    * Update README.md
    
    * Update README.md
    
    * Update customize.sh

---
+++ autogen-icon.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+CREW_PREFIX=$2
+PWA_PREFIX=$CREW_PREFIX/lib/pwa
+appname=$3
+icon=$1
+webpath=$4
+path=$CREW_PREFIX/share/pixmaps
+icon () { ls -1 $path 2> /dev/null | grep $icon; }
+num=`icon | wc -l`
+if [[ "$num" != "0" ]]
+then
+if [[ "$num" != "1" ]]
+then
+echo "$num icons were found for $appname"
+icon
+read -r -p "Which icon you want to use?" appname_response
+iconpath=$path/$appname_response
+else
+iconpath=$path/`icon`
+fi
+if [[ $iconpath = *.xpm ]]
+then
+convert $iconpath ${iconpath}.png
+cp ${iconpath}.png $PWA_PREFIX/$webpath/templates/icon/brew.png
+rm ${iconpath}.png
+else
+cp $iconpath $PWA_PREFIX/$webpath/templates/icon/brew.png
+fi
+echo "Icon for $appname deployed!"
+else
+echo "No icon were found for $appname :/"
+fi
