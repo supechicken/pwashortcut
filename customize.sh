80321007b1cac56a09c7d5065ce9d5712b8acc9f
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-01-01 16:47:41 +0800

    Turning into Websocket

--- customize.sh
+++ customize.sh
@@ -18,12 +18,12 @@ esac
 ################################################
 echo "Making a customize PWA configuration"
 read -p "Application Name: " appname
-if [[ `echo $APP | grep $appname` != "" ]] 
+if [[ $(echo $APP | grep $appname) != '' ]] 
 then
     read -r -p "${appname^} has a preinstalled customize theme, do you wanna use it? [y/N] " appname_response
     case "$appname_response" in
       [yY][eE][sS]|[yY]) 
-        cp $icon_dir/$appname.json $PWA_PREFIX/$appname/templates/manifest.json
+        cp $icon_dir/$appname.json ~/.extension/apps/$appname/$appname.json
         echo "Using preinstalled configuration"
         exit 0
         ;;
@@ -33,6 +33,6 @@ read -p "Enter path of the icon you want to use in ${appname^}:" icon_path
 read -p "Enter the window color of PWA you want to use in HEX (without #):" window_color
 read -p "Enter the background color of PWA you want to use in HEX (without #):" background_color
 cp $icon_path $PWA_PREFIX/$appname/icon/brew.png
-sed 's/000000/$window_color/1' $PWA_PREFIX/$appname/templates/manifest.json
-sed 's/000000/$background_color/2' $PWA_PREFIX/$appname/templates/manifest.json
+sed 's/000000/$window_color/1' ~/.extension/apps/$appname/$appname.json
+sed 's/000000/$background_color/2' ~/.extension/apps/$appname/$appname.json
 echo "A customize PWA configuration file for ${appname^} created!"
